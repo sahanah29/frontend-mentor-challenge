b535686d8819c9bf25c5342b00a94e8f6618ca33
Sahana Hiremath <69656120+sahanah29@users.noreply.github.com>
2023-10-08 14:02:19 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,31 @@
+function featuresDropdown(){
+    console.log("features dropdown clicked");
+    document.getElementById("featuresList").classList.toggle("show");
+}
 
+function companyDropdown(){
+    console.log("company dropdown clicked");
+    document.getElementById("companyList").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+
+    if (document.getElementById("featuresList").classList.contains("show") && !event.target.matches('#featuresDrop')) {
+        document.getElementById("featuresList").classList.remove("show");}
+    if (document.getElementById("companyList").classList.contains("show") && !event.target.matches('#companyDrop')) {
+        document.getElementById("companyList").classList.remove("show");
+    }
+
+  }
+
+function mobileNav(){
+    console.log("mobile nav");
+    document.getElementById("mobile-nav").hidden = false;
+}
+
+function closeButton(){
+    document.getElementById("mobile-nav").hidden = true;
+}
+
+
+  
